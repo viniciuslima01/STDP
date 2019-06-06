a16b7a3d7df745c72f0590f613a3ce9ba90c9e35
viniciuslima01 <50642428+viniciuslima01@users.noreply.github.com>
2019-06-06 19:34:03 -0300

    Create detector_hog.py

---
+++ projetoplacas/detector_hog.py
@@ -0,0 +1,21 @@
+
+import cv2
+import dlib
+
+
+imagem = cv2.imread("fotos/grupo.5.jpg")
+detector = dlib.get_frontal_face_detector()
+facesDetectadas = detector(imagem, 4)
+for face in facesDetectadas:
+	#print(face)
+	#print(face.left())
+	#print(face.top())
+	#print(face.right())
+	#print(face.bottom())
+	e, t, d, b = (int(face.left()), int(face.top()), int(face.right()), int(face.bottom()))
+	cv2.rectangle(imagem, (e, t), (d, b), (0, 255, 255), 1)
+
+cv2.imshow("Detector hog", imagem)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
